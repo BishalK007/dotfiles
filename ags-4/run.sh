880e5773ff48662b1d2935d5e7d1e5dce69b15ff
Bishal <bishal@bishal.pro>
2025-05-04 13:24:53 +0530

    FIX run.sh file

--- ags-4/run.sh
+++ ags-4/run.sh
@@ -1,1 +1,5 @@
-watchexec -w . --exts js,ts,jsx,tsx,css,scss --restart -- sh -c 'pkill -HUP ags || GTK_DEBUG=interactive ags run --gtk4 -d .'
+if [[ "$@" == *"--debug"* ]]; then
+    watchexec -w $(realpath /home/bishal/.config/ags) --exts js,ts,jsx,tsx,css,scss --restart -- sh -c 'pkill -HUP ags || GTK_DEBUG=interactive ags run --gtk4 -d $(realpath /home/bishal/.config/ags)'
+else
+    watchexec -w $(realpath /home/bishal/.config/ags) --exts js,ts,jsx,tsx,css,scss --restart -- sh -c 'pkill -HUP ags || ags run --gtk4 -d $(realpath /home/bishal/.config/ags)'
+fi
